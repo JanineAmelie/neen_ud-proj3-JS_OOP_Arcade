d93ecc5889acf7f08f6a705066a5f549ec98020f
Neen <tamarind11@yahoo.com>
2016-11-28 02:24:27 +0800

    feat: collision fix

--- js/app.js
+++ js/app.js
@@ -18,23 +18,25 @@ var Enemy = function(howfast, rownum) {
     this.speed = howfast;
 };
 
+//
 Enemy.prototype.Yrow = function(yvaluenum) {
-        var currentRow;
+    var currentRow;
 
-        if (yvaluenum > 60 && yvaluenum < 140) {
-            currentRow = 1;
-            return currentRow;
+    if (yvaluenum > 60 && yvaluenum < 140) {
+        currentRow = 1;
+        return currentRow;
 
-        } else if (yvaluenum > 140 && yvaluenum < 220) {
-            currentRow = 2;
-            return currentRow;
+    } else if (yvaluenum > 140 && yvaluenum < 220) {
+        currentRow = 2;
+        return currentRow;
 
-        } else if (yvaluenum > 220 && yvaluenum < 310) {
-            currentRow = 3;
-            return currentRow;
-        }
+    } else if (yvaluenum > 220 && yvaluenum < 310) {
+        currentRow = 3;
+        return currentRow;
     }
-    // Parameter: dt, a time delta between ticks
+}
+
+// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     //Modulo resets to 0 when x = a multiple of n
     // In this case n = 505 or 5*(width of the cell)
@@ -42,10 +44,10 @@ Enemy.prototype.update = function(dt) {
     this.x = (this.x + stepSize) % 505;
 
     if (this.collision() == true) {
-    //endGame();
-    //player.resetPos();
-    console.log("collision")
-    //restartMSG("Oop! Enemy collision! Game Over", "Start a new game?")
+        //endGame();
+        player.resetPos();
+        console.log("collision")
+            //restartMSG("Oop! Enemy collision! Game Over", "Start a new game?")
 
     }
 
@@ -53,36 +55,58 @@ Enemy.prototype.update = function(dt) {
 
 Enemy.prototype.collision = function() {
     //ei --> enemyIndex
-    var collided;
+    var collided = false;
     for (var ei = 0; ei < allEnemies.length; ei++) {
 
         var enem = allEnemies[ei];
-        //Enemy variables
+
+//We need to compute the actual bounding box
+//of the character sprite for proper collision
+//because the enemy and player image sprite
+//in the images folder has white space around it.
+//see my visual explanation of this problem at this link:
+//https://www.dropbox.com/s/vllmjpfqud0opko/3_explanationforSprite-collision.jpg?raw=1
+
+        var imageHeight = 171;
+        var imageWidth = 101
+
+        /* Enemy bounding box computation */
+
+        //enemy actual width = 101px same as imagewidth
+        var enemyHeight = 65;
+        var enemyBotSpace = 28;
+
         var x1 = enem.x;
-        var x2 = enem.x + 101;
-        var y1 = enem.y;
-        var y2 = enem.y + 40;
+        var x2 = enem.x + imageWidth;
+        var y1 = enem.y + (imageHeight - (enemyHeight + enemyBotSpace));
+        var y2 = enem.y + (imageHeight - enemyBotSpace);
 
-        //player variables
-        var a1 = player.x;
-        var a2 = player.x + 101;
-        var b1 = player.y;
-        var b2 = player.y + 80;
+        /* Player bounding box computation*/
+        var charHeight = 75;
+        var charWidth = 66;
+        var charSideSpace = 18;
+        var charBotSpace = 32;
 
+        var a1 = player.x + (imageWidth - (charWidth + charSideSpace));
+        var a2 = player.x + (imageWidth - charSideSpace);
+        var b1 = player.y + (imageHeight - (charHeight + charBotSpace));
+        var b2 = player.y + (imageHeight - charBotSpace);
+
+        /*Checking for Collisions */
         //X VALUES
-        if       (x1 < a1 && x1 < a2 && x2 < a1 && x2 < a2) {
-            collided = false;
-        }else if (x1 > a1 && x1 > a2 && x2 > a1 && x2 > a2) {
-            collided = false;
+        if (x1 < a1 && x1 < a2 && x2 < a1 && x2 < a2) {
+            //collided = false;
+        } else if (x1 > a1 && x1 > a2 && x2 > a1 && x2 > a2) {
+            //collided = false;
 
-        //Y Values
-        }else if (y1 < b1 && y1 < b2 && y2 < b1 && y2 < b2) {
-            collided = false;
-        }else if (y1 > b1 && y1 > b2 && y2 > b1 && y2 > b2) {
-            collided = false;
+            //Y Values
+        } else if (y1 < b1 && y1 < b2 && y2 < b1 && y2 < b2) {
+            //collided = false;
+        } else if (y1 > b1 && y1 > b2 && y2 > b1 && y2 > b2) {
+            //collided = false;
 
-        //Otherwise there is collision
-        }else {
+            //Otherwise there is collision
+        } else {
             collided = true;
         }
     }
@@ -95,11 +119,15 @@ Enemy.prototype.render = function() {
 };
 
 var Player = function() {
+    //starting coordinates
     this.x = 202;
     this.y = 390;
+
     this.stepX = 100; //stepsize along X
     this.stepY = 80; //stepsize along Y
-    this.sprite = 'images/char-stan.png';
+
+    //current sprite
+    this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.Yrow = function(yvaluenum) {
@@ -185,9 +213,11 @@ Player.prototype.update = function(direction) {
     }
 };
 
+//renders the player sprite
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
 
 Player.prototype.handleInput = function(keyinput) {
     console.log(keyinput);
@@ -195,6 +225,7 @@ Player.prototype.handleInput = function(keyinput) {
 
 };
 
+//coordinates where the player should be placed when this method is called
 Player.prototype.resetPos = function() {
     this.x = 202;
     this.y = 390;
@@ -215,38 +246,6 @@ Player.prototype.waterContact = function() {
     };
 }
 
-/*TODO: ARRAY of 15 spaces for the items/collectibles to spawn in. random generated spot */
-actionArea = [
-    //from the bottom
-    //row 1
-    //x   //y
-    [2, 230], //0
-    [102, 230], // 1
-    [202, 230], // 2
-    [302, 230], // 3
-    [402, 230], // 4
-    //row 2
-    [2, 150], // 5
-    [102, 150], // 6
-    [202, 150], // 7
-    [302, 150], // 8
-    [402, 150], // 9
-    //row 3
-    [2, 70], //  10
-    [102, 70], //  11
-    [202, 70], //  12
-    [302, 70], //  13
-    [402, 70] //  14
-]
-
-// if  the player's entire occupied space coordinates:
-// if this.x
-
-//[range: this.x to (this.x + width of sprite)  and this.y to (this.y + height of sprite ]  <- calculated at update method.
-//falls anywhere between the enemy's occupied space:
-// [range: this.x to (this.x + width of sprite)  and this.y to (this.y + height of sprite] <- calculated at upate method.
-//collision will occur.
-
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -257,7 +256,6 @@ var player = new Player();
 allEnemies.push(new Enemy(200, 1), new Enemy(100, 2), new Enemy(150, 3))
 
 // This listens for key presses and sends the keys to your Player.handleInput() method.
-
 document.addEventListener('keyup', function(e) {
     if (timerStarted == true) {
         var allowedKeys = {
--- js/engine.js
+++ js/engine.js
@@ -241,8 +241,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png',
-        'images/char-stan.png'
+        'images/char-boy.png'
     ]);
     Resources.onReady(init);
 
